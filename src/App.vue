afd815e6103fcf27294c2dcca7dc1f95340b8a4e
= <hugo.poullet@epitech.eu>
2019-05-03 17:38:27 +0200

    Adding VueX store

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+	<div id="nav">
+	  <router-link to="/">Home</router-link> |
+	  <router-link to="/about">About</router-link>
+	</div>
+	<router-view/>
   </div>
 </template>
 
@@ -19,11 +19,28 @@
 #nav {
   padding: 30px;
   a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+	font-weight: bold;
+	color: #2c3e50;
+	&.router-link-exact-active {
+	  color: #42b983;
+	}
   }
 }
 </style>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { Action } from "vuex-class";
+
+@Component
+	export default class App extends Vue {
+		@Action('initStore') initStore:any
+		mounted() {
+			this.initStore()
+		}
+	}
+
+
+</script>
+
--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,23 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios, { AxiosResponse } from 'axios';
 
 Vue.use(Vuex);
 
+
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+	state: {
+		headers: Array
+	},
+	mutations: {
+		SET_HEADERS: (state, payload) => {
+			state.headers = payload
+		}
+	},
+	actions: {
+		async initStore({ commit }) {
+			const res: AxiosResponse = await axios.get(`${process.env.VUE_APP_API_URL}headers/headers.json`)
+			commit('SET_HEADERS', res.data)
+		}
+	},
 });
